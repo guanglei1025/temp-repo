c148a7b753f2c757e32c47ae61cb6bd7f50a870a
Guanglei Liu <guanglei1025@gmail.com>
2023-01-17 17:49:12 -0500

    created project

---
+++ temp-project/temp-project/temp_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  temp_projectApp.swift
+//  temp-project
+//
+//  Created by Guanglei Liu on 1/17/23.
+//
+
+import SwiftUI
+
+@main
+struct temp_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
